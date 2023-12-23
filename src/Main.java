e657f048597714d014f4b80efbee8a5271f695d3
viktorivanov56 <you@example.com>
2023-12-23 22:03:43 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Viktor");
+        int number =3;
+        System.out.println(number);
+        int nomer =12;
+        int numb =6;
+        int numberr =5;
+        System.out.println(nomer + numb - numberr);
+    }
+}
