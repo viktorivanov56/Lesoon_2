b712227f3832fdbb45b22ff2763635e4fde87cc5
viktorivanov56 <you@example.com>
2023-12-23 21:15:09 +0200

    refactor code

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Viktor");
-        int number =3;
+        int number = 3;
         System.out.println(number);
-        int nomer =12;
-        int numb =6;
-        int numberr =5;
+        int nomer = 12;
+        int numb = 6;
+        int numberr = 4;
         System.out.println(nomer + numb - numberr);
     }
 }
